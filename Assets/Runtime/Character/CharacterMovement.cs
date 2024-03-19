242d9ac77de8bc19993ef17f393dfb836df96055
Brevillo <oliver.beebe@digipen.edu>
2024-03-19 16:42:55 -0700

    Created first critter with basic wandering movemnt and improved character movement

--- Assets/Runtime/Character/CharacterMovement.cs
+++ Assets/Runtime/Character/CharacterMovement.cs
@@ -7,27 +7,41 @@ namespace MystiCorp.Runtime.Character
         [SerializeField]
         private float moveSpeed;
         [SerializeField]
+        private float positionPrecision = 0.1f;
+        [SerializeField]
         private new Rigidbody2D rigidbody;
 
         private Vector2 targetPosition;
-        private bool moving;
+
+        public bool Moving { get; private set; }
 
         public void MoveTo(Vector2 position)
         {
+            if (rigidbody.position == position) return;
+
             this.targetPosition = position;
-            this.moving = true;
+            this.Moving = true;
         }
 
         public void Stop()
         {
-            this.moving = false;
+            this.Moving = false;
         }
 
         private void Update()
         {
-            Vector2 direction = (this.targetPosition - (Vector2)this.transform.position).normalized;
+            Vector2 targetDelta = this.targetPosition - (Vector2)this.transform.position;
+            Vector2 direction = targetDelta.normalized;
 
-            if (this.moving) this.rigidbody.velocity = direction * this.moveSpeed;
+            // if reached target
+            if (targetDelta.magnitude < positionPrecision)
+            {
+                rigidbody.MovePosition(targetPosition);
+                Moving = false;
+                rigidbody.velocity = Vector2.zero;
+            }
+
+            if (this.Moving) this.rigidbody.velocity = direction * this.moveSpeed;
         }
     }
 }
