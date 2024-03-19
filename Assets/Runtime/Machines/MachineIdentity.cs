86d11a0bf2341309feec4f0880a18370c4290e43
Travis French <travis.french.1991@gmail.com>
2024-03-19 15:10:07 -0700

    Renamed machine magnitude and cycle time fields for better clarity.

--- Assets/Runtime/Machines/MachineIdentity.cs
+++ Assets/Runtime/Machines/MachineIdentity.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using Attribute = TravisRFrench.Attributes.Runtime.Attribute;
 
 namespace MystiCorp.Runtime.Machines
@@ -7,13 +8,13 @@ namespace MystiCorp.Runtime.Machines
     {
         [Tooltip("How potent should the effects of this machine be?")]
         [SerializeField]
-        private float magnitude = 1f;
+        private float baseMagnitude = 1f;
         [Tooltip("How many times does this machine activate per second?")]
         [SerializeField]
-        private float cycleTime = 1f;
+        private float baseCycleTime = 1f;
         
-        public float Magnitude => this.magnitude * this.MagnitudeMultiplier.ModifiedValue;
-        public float CycleTime => this.cycleTime * this.CycleTimeMultiplier.ModifiedValue;
+        public float Magnitude => this.baseMagnitude * this.MagnitudeMultiplier.ModifiedValue;
+        public float CycleTime => this.baseCycleTime * this.CycleTimeMultiplier.ModifiedValue;
 
         [field: SerializeField]
         public Attribute MagnitudeMultiplier { get; private set; }
