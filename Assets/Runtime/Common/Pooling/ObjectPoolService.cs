04bbe8a35a540110634a913b199385d8dc94c1ff
Brevillo <oliver.beebe@digipen.edu>
2024-03-21 21:05:19 -0700

    Switched pools to not be services

--- Assets/Runtime/Common/Pooling/ObjectPoolService.cs
+++ Assets/Runtime/Common/Pooling/ObjectPoolService.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MystiCorp.Runtime.GameManager;
 using UnityEngine.Pool;
 
 namespace MystiCorp.Runtime
 {
-    public abstract class ObjectPoolService : GameServiceManager.Service
+    public abstract class ObjectPoolService : ScriptableObject
     {
         [SerializeField]
         private GameObject prefab;
@@ -17,6 +16,8 @@ namespace MystiCorp.Runtime
 
         protected GameObject GetObject()
         {
+            if (pool == null || poolParent == null) InitializePool();
+
             var obj = pool.Get();
 
             obj.SetActive(true);
@@ -33,21 +34,28 @@ namespace MystiCorp.Runtime
             pool.Release(obj);
         }
 
-        protected override void RuntimeInitializeOnLoad()
-        {
-            InitializePool();
-        }
-
         private void InitializePool()
         {
+            // dispose if active
+            if (poolParent != null) Destroy(poolParent.gameObject);
+            pool?.Dispose();
+
+            // initialize
             poolParent = new GameObject(GetType().Name).transform;
             activeObjects = new();
-            pool = new(CreateObject);
+            pool = new(
+                createFunc: CreateObject,
+                actionOnDestroy: DestroyObject);
         }
 
         protected virtual GameObject CreateObject()
         {
             return Instantiate(prefab, poolParent);
+        }
+
+        protected virtual void DestroyObject(GameObject obj)
+        {
+            Destroy(obj);
         }
     }
 }
